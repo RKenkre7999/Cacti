20e68a06d2eff71f5f3fe2cede048ae75328208e
RKenkre7999 <43531962+RKenkre7999@users.noreply.github.com>
2018-09-24 10:22:32 +0400

    This is a custom project
    try the serialization

---
+++ gulp-config.js
@@ -0,0 +1,17 @@
+module.exports = function () {
+    var instanceRoot = "C:\\inetpub\\wwwroot\\CactiTest";
+  var config = {
+    websiteRoot: instanceRoot + "\\Website",
+    sitecoreLibraries: instanceRoot + "\\Website\\bin",
+    licensePath: instanceRoot + "\\Data\\license.xml",
+    solutionName: "Habitat",
+    buildConfiguration: "Debug",
+    buildToolsVersion: "auto",
+    buildMaxCpuCount: 0,
+    buildVerbosity: "minimal",
+    buildPlatform: "Any CPU",
+    publishPlatform: "AnyCpu",
+    runCleanBuilds: false
+  };
+  return config;
+}
